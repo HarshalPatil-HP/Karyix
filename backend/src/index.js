8fbccca9f9ccb06f55d4184d73e7f96fa547a85d
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-07-15 23:55:35 +0530

    server is now listinibng

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,21 @@
 import dotenv from "dotenv";
-
+import express from "express";
 dotenv.config({
     path: "./.env",
     override: true
 });
+const port=process.env.PORT || 7000;
+const app=express()
+
+app.get('/',(req,res)=>{
+    console.log("so its working")
+    res.send("so its working")
+})
+
+app.listen(port,()=>{
+    console.log(`the website is on http://localhost:${port}`)
+})
+
 
 
 
