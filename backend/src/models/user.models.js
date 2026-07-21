f21b3d5572309a8da795524edf79c5229528de0f
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-07-22 00:28:41 +0530

    methods are implemented to check passwords

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -66,6 +66,10 @@ userSchema.pre("save",async function(next) {
 
     this.password=bcrypt.hash(this.password,10);
     next();
-})
+});
+
+userSchema.methods.isPasswordCorrect= async function (next) {
+    return await bcrypt.compare(password,this.password);
+};
 
 export const user=mongoose.model("User",userSchema);
