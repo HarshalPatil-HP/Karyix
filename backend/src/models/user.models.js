9b7cf5e3032fb03d8a37c35376f440c408e06cc5
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-07-22 00:11:43 +0530

    initialized password hooks

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,5 +1,5 @@
 import { Schema } from "mongoose";
-
+import bcrypt from "bcrypt";
 
 const userSchema=new Schema(
     {
@@ -60,3 +60,12 @@ const userSchema=new Schema(
         timestamps:true,
     },
 );
+
+userSchema.pre("save",async function(next) {
+    if(!this.isModified("password")) return next();
+
+    this.password=bcrypt.hash(this.password,10);
+    next();
+})
+
+export const user=mongoose.model("User",userSchema);
