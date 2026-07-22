b413fd690ea70cf2e093420cd793e34cdf4ca258
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-07-22 22:38:43 +0530

    implemented jwt securitty and also crypto tokens for validate pupose use

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,5 +1,7 @@
 import { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema=new Schema(
     {
@@ -71,5 +73,53 @@ userSchema.pre("save",async function(next) {
 userSchema.methods.isPasswordCorrect= async function (next) {
     return await bcrypt.compare(password,this.password);
 };
+
+userSchema.methods.getAccessToken=function (){
+    return jwt.sign(
+        {
+         _id:this._id,
+         email:this.email,
+         username:this.username   
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+}
+userSchema.methods.getRefreshToken=function (){
+    return jwt.sign(
+        {
+         _id:this._id   
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+
+    )
+}
+
+userSchema.methods.generateTemproryToken=function(){
+    const unhashed=crypto.randomBytes(10).toString("hex")
+
+    console.log(unhashed);
+
+    const hashed=crypto
+                .createHash("sha256")
+                .update(unhashed)
+                .digest("hex")
+    
+    const tokenExpiry=Date.now()+(20*60*1000)    //20m
+    console.log(hashed);
+    
+    return {
+        unhashed,
+        hashed,
+        tokenExpiry
+    };
+}
+
 
 export const user=mongoose.model("User",userSchema);
