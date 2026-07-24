489c7c3b177505f9124ed0206a7d1199c29b8667
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-07-24 19:14:06 +0530

    sne dmail added now we can send mails

--- backend/src/utils/mail.js
+++ backend/src/utils/mail.js
@@ -1,42 +1,74 @@
 import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+import { MailtrapTransport } from "mailtrap";
 
-const emailVerificationContend=(username,verifyUrl)=>{
-    return{
-        body: {
-        name: username,
-        intro: 'Welcome to our Website! Were very excited to have you on board.',
-        action: {
-            instructions: 'To get started with Verify, please click here:',
-            button: {
-                color: '#22BC66', 
-                text: 'Verify your account',
-                link: verifyUrl
-            }
-        },
-        outro: 'Need help, or have questions? Just reply to this email, we love to help.'
-       }
-    }
-}
+const mailGenerator = new Mailgen({
+  theme: "default",
+  product: {
+    name: "Your Website",
+    link: "https://yourwebsite.com/",
+  },
+});
 
-const forgotPasswordContend=(username,passwordUrl)=>{
-    return{
-        body: {
-        name: username,
-        intro: 'Welcome to our Website!',
-        action: {
-            instructions: 'To change Your Password, please click here:',
-            button: {
-                color: '#22BC66', 
-                text: 'Change  your  password',
-                link: passwordUrl
-            }
-        },
-        outro: 'Need help, or have questions? Just reply to this email, we love to help.'
-       }
-    }
-}
+const emailVerificationContent = (username, verifyUrl) => ({
+  body: {
+    name: username,
+    intro: "Welcome to our Website! We're very excited to have you on board.",
+    action: {
+      instructions: "To get started, please click here:",
+      button: {
+        color: "#22BC66",
+        text: "Verify your account",
+        link: verifyUrl,
+      },
+    },
+    outro: "Need help, or have questions? Just reply to this email, we love to help.",
+  },
+});
 
-export {
-    emailVerificationContend,
-    forgotPasswordContend
-}
+const forgotPasswordContent = (username, passwordUrl) => ({
+  body: {
+    name: username,
+    intro: "Welcome to our Website!",
+    action: {
+      instructions: "To change your password, please click here:",
+      button: {
+        color: "#22BC66",
+        text: "Change your password",
+        link: passwordUrl,
+      },
+    },
+    outro: "Need help, or have questions? Just reply to this email, we love to help.",
+  },
+});
+
+const sendEmail = async ({ to, subject, mailGenContent }) => {
+  const TOKEN = process.env.IMP_TOKEN;
+
+  const transport = nodemailer.createTransport(
+    MailtrapTransport({ token: TOKEN })
+  );
+
+  const sender = {
+    address: "hello@demomailtrap.co",
+    name: "Karyix",
+  };
+
+  const html = mailGenerator.generate(mailGenContent);
+  const text = mailGenerator.generatePlaintext(mailGenContent);
+
+  try {
+    await transport.sendMail({
+      from: sender,
+      to,
+      subject,
+      text,
+      html,
+    });
+  } catch (error) {
+    console.error("Email sending failed:", error);
+    throw error;
+  }
+};
+
+export { emailVerificationContent, forgotPasswordContent, sendEmail };
